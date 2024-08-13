5815647d799f394a0b95d16c2f7bae43c5370211
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2024-08-13 22:47:23 +0300

    Update MXS.py

--- MXS.py
+++ MXS.py
@@ -9,12 +9,13 @@ argparser = argparse.ArgumentParser(description="Mass XSS Scanner")
 argparser.add_argument("-i", "--input", help="URLs to scan", type=str, required=True)
 argparser.add_argument("-o", "--output", help="Output file", type=str, default="vulnerable_urls.txt")
 argparser.add_argument("-c", "--concurrency", help="Number of concurrent requests", type=int, default=50)
-argparser.add_argument("-t", "--timeout", help="Request timeout", type=float, default=4)
+argparser.add_argument("-t", "--timeout", help="Request timeout", type=float, default=15)
+argparser.add_argument("-p", "--payload", help="XSS payload File (Only one payload)", type=str, required=False)
 argparser.add_argument("-x", "--hidden", help="Redact domains in the terminal output", action="store_true")
 
 args = argparser.parse_args()
 
 
 if __name__ == "__main__":
-    scanner = MassScanner(args.input, args.output , args.concurrency, args.timeout, args.hidden)
+    scanner = MassScanner(args.input, args.output , args.concurrency, args.timeout, args.payload, args.hidden)
     scanner.run()
